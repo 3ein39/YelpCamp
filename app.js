6c1a74de38455b39029a876e96b6a45e34698509
Hussein Hany <hhany3997@gmail.com>
2021-12-30 22:04:47 +0200

    Define Campground model

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const Campground = require('./models/campground')
+
+const mongoDB = 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('database connected')
+});
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makeCamp', async (req, res) => {
+    const camp = new Campground({ title: 'My backyard', description: 'A great place' });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
