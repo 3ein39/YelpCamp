b41d7ebc3f44dd45525f4ef76e59472adf70d611
Hussein Hany <hhany3997@gmail.com>
2022-01-09 12:01:58 +0200

    intialize ejs-mate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
 const path = require('path');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -15,7 +16,7 @@ db.once('open', () => {
     console.log('database connected');
 });
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
