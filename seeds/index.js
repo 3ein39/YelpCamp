4910060c1520951d9e25e565740ab0319c8ff31a
Hussein Hany <hhany3997@gmail.com>
2022-01-10 22:09:29 +0200

    Implement basic bootstrap styles

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const axios = require('axios');
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const { descriptors, places } = require('./seedsHelper')
@@ -14,14 +15,32 @@ db.once('open', () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+async function seedImg() {
+    try {
+        const resp = await axios.get('https://api.unsplash.com/photos/random', {
+            params: {
+                client_id: 'h3jg6e9i6Ofw9lfoREB7k07a5qutx7u9NlPurQwn6fc',
+                collections: 483251,
+            },
+        })
+        return resp.data.urls.small
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 20; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: await seedImg(),
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae aliquid voluptatem dignissimos omnis impedit, molestias architecto magnam doloremque distinctio, repellendus perspiciatis earum similique, quos nobis ipsum qui veritatis aliquam praesentium!',
+            price
         })
         await camp.save();
     }
