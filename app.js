035bdac3ae2fe1d424e9f6c6a23f3da120f07a96
Hussein Hany <hhany3997@gmail.com>
2022-01-26 14:10:35 +0200

    Configure session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const path = require('path');
 const mongoose = require('mongoose');
 const ExpressError = require('./utils/ExpressError');
@@ -28,6 +29,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+app.use(session(sessionConfig));
+
 
 
 app.use('/campgrounds', campgroundsRouter);
