94c12e5ab762741f459b79d2f858e04641398375
Hussein Hany <hhany3997@gmail.com>
2022-01-31 22:14:17 +0200

    Define returnTo behavior

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,9 @@ router.post('/login', passport.authenticate('local', {
     failureRedirect: '/login'
 }), async (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res) => {
