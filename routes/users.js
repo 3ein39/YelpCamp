4d350b2353ac7fff2b0542c8a73becb6e37957ff
Hussein Hany <hhany3997@gmail.com>
2022-02-02 15:58:23 +0200

    Restructure Routes

--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,16 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require("passport");
 const users = require('../controllers/users')
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+    .get(users.renderRegisterForm)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login'
-}), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login'
+    }), users.login);
 
 router.get('/logout', users.logout)
 
