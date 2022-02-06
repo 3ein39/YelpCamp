a5622ed297fedbec18d9a6180fbc67796f7ea761
Hussein Hany <hhany3997@gmail.com>
2022-02-06 18:33:49 +0200

    Add start script

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 });
 
-
-app.listen(3000, () => {
-    console.log("Example app listening at port")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving at port: ${port}`);
 });
